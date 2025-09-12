084148f3cc9f703d99c9b2befde414d687ab5b23
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2025-09-12 08:49:39 -0500

    Creating App

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Day Goes Here</Text>
+      <TextInput></TextInput>
+
+    </View>
+  );
+}
