25a789e5498074bdcc59a5e7fee1bbbda394f963
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2025-09-12 12:59:09 -0500

    things

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import { Text, TextInput } from 'react-native';
+import { View } from 'react-native-reanimated/lib/typescript/Animated';
+
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+
+      <View>
+      </View>
+
+      {/* Prime Number
+      {isPrime && (
+        <View>
+          <Text>Prime Number</Text>
+        </View>
+
+      )} */}
+
+
+
+      <Text>Day Goes Here</Text>
+      <TextInput></TextInput>
+
+    </View>
+  );
+}
