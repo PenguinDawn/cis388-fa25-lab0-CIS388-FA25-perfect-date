bd720fb7ad09f5665742b1f2d587154ede45c66b
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2025-09-12 08:54:15 -0500

    Create yourdate.tsx

---
+++ app/yourdate.tsx
@@ -0,0 +1,19 @@
+import { Text, TextInput, View } from "react-native";
+
+
+export default function YourDate() {
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Day Goes Here</Text>
+      <TextInput></TextInput>
+
+    </View>
+  );
+}
